557e94e6bede73f56022f5e88a58bcf348b88bc0
Ndolo S <shadrack.winters@outlook.com>
2017-08-14 13:18:33 +0300

    Adding an assets folder

---
+++ README.md
@@ -0,0 +1,2 @@
+# This is my first repo's readme file
+# Anita-s-Kitchen
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//   $("form#add").submit(function(event) {
+//     event.preventDefault();
+//     var number1 = parseInt($("#add1").val());
+//     var result = add(number1);
+//     $("#output").text(result);
+//   });
+// });
